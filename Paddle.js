4a8961129ffe851d43c75d428db3501ac537fc9d
MohanKishan <65703437+MohanKishan@users.noreply.github.com>
2021-06-04 18:07:27 +0530

    Add files via upload

---
+++ Paddle.js
@@ -0,0 +1,17 @@
+
+
+//whitehat this gmae is not done pls wait 
+class Paddle {
+  
+  constructor(){
+    this.width = 10;
+    this.height = 70;
+    this.xPosition = 0;
+    this.yPosition= 0;
+  }
+  
+  
+  display(){
+  rect(this.xPosition, this.yPosition,this.width,this.height);
+ }
+}
